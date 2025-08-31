456680631263c3d36a627d7b828f6e9098d70e3e
Sriram2524 <sriramsriram2316@gmail.com>
2025-08-31 16:14:58 +0530

    your message here

--- server/index.ts
+++ server/index.ts
@@ -61,10 +61,11 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = 5000;
   server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  port,
+  host: "127.0.0.1",  // or "localhost"
+  // reusePort: true,  <-- remove this line
+}, () => {
+  log(`serving on http://localhost:${port}`);
+});
+;
 })();
